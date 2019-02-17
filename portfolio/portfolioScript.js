b148cf1844bdc7a658548f8ec2cc9ca87cc23a48
jshanneson <45860628+jshanneson@users.noreply.github.com>
2019-02-17 00:45:19 -0500

    added a portfolio page

---
+++ portfolio/portfolioScript.js
@@ -0,0 +1,13 @@
+//alert('file linked')
+
+//number
+
+//string
+
+//array
+//var colors = ['red', 'blue', 'green'];
+//var colors = new Array('red', 'yellow', 'orange');
+//colors.push('purple');
+
+//alert(colors.sort());
+//object
