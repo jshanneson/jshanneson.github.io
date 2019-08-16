34993e5e2e068531edfe9a53ffbeb42f2757d24d
Joshua Hanneson <jshanneson@users.noreply.github.com>
2019-08-15 20:02:21 -0400

    updating template, building newsite html

---
+++ template/script.js
@@ -0,0 +1,10 @@
+//alert("connected");
+//var grid = document.getElementById("grid");
+
+document.getElementById("grid").onscroll 
+= function() {scrollDown()};
+
+function scrollDown() {
+	alert("you scrolled");
+	//window.scrollTo(0, 1080);
+}
